47808d768709dd87d6e7736c54977f901c17fd86
Opeyemi Balogun <jpeyemi@MacBook-Pro-4.local>
2022-05-26 17:12:04 -0400

    touch keyboard

--- client/src/components/pages/Play.js
+++ client/src/components/pages/Play.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./Play.css";
 import Row from "../modules/Row.js";
+//import { Button } from "react-bootstrap";
 
 const Play = () => {
     let rows = null;
@@ -12,8 +13,59 @@ const Play = () => {
         />
         //creates row features
     ));
+    let keysR1 = ('QWERTYUIOP').split('')
+    let keysR2 = ('ASDFGHJKL').split('')
+    let keysR3 = ('1ZXCVBNM0').split('')
 
+    const handleClick = (x) => {
+        console.log(x);
+    }
+    let keyboardR1 = keysR1.map((w) => (
+        <div id={w} value = {w} onClick = {() => {handleClick(w)}} className = "Play-KeyTile">
+            {w}
+        </div>
+        
+    ));
+    let keyboardR2 = keysR2.map((w) => (
+        <div value = {w} onClick = {() => {handleClick(w)}} className = "Play-KeyTile">
+            {w}
+        </div>
+    ));
 
+    const Row3 = (x) => {
+        let bttn = null
+        if(x == 0){
+            bttn = (
+                <div id = {x} onClick = {() => {handleClick(x)}} className = "Play-KeyTile Back">
+                    Back
+                </div>
+            )
+        }else if(x == 1){
+            bttn = (
+                <div name = {x} id = '1' onClick = {() => {handleClick(x)}} className = "Play-KeyTile Enter">
+                    Enter
+                </div>
+            )
+        }else{
+            bttn = (
+            <div id = {x} onClick = {() => {handleClick(x)}} className = "Play-KeyTile">
+                {x}
+            </div>
+            )
+        }
+        /*console.log(bttn)
+        document.getElementsByClassName("Play-KeyTile").addEventListener("keydown", (event) => {
+            if(event.key == x){
+                document.getElementById(x).click();
+            }
+        }, false);*/
+        return(bttn);
+
+    }
+    let keyboardR3 = keysR3.map((w) => (
+        Row3(w)
+    ));
+    //console.log(document.getElementsById('1'));
     return(
         <>
         <div className="Play-Container">
@@ -64,94 +116,13 @@ const Play = () => {
             <div className = "Play-KeyboardContainer">
                 <div className = "Play-Keyboard">
                     <div className = "Play-KeyboardRow1">
-                        <div className = "Play-KeyTile">
-                            Q
-                        </div>
-                        <div className = "Play-KeyTile">
-                            Q
-                        </div>
-                        <div className = "Play-KeyTile">
-                            Q
-                        </div>
-                        <div className = "Play-KeyTile">
-                            Q
-                        </div>
-                        <div className = "Play-KeyTile">
-                            Q
-                        </div>
-                        <div className = "Play-KeyTile">
-                            Q
-                        </div>
-                        <div className = "Play-KeyTile">
-                            Q
-                        </div>
-                        <div className = "Play-KeyTile">
-                            Q
-                        </div>
-                        <div className = "Play-KeyTile">
-                            Q
-                        </div>
-                        <div className = "Play-KeyTile">
-                            Q
-                        </div>
+                        {keyboardR1}
                     </div>
                     <div className = "Play-KeyboardRow2">
-                        <div className = "Play-KeyTile">
-                            Q
-                        </div>
-                        <div className = "Play-KeyTile">
-                            Q
-                        </div>
-                        <div className = "Play-KeyTile">
-                            Q
-                        </div>
-                        <div className = "Play-KeyTile">
-                            Q
-                        </div>
-                        <div className = "Play-KeyTile">
-                            Q
-                        </div>
-                        <div className = "Play-KeyTile">
-                            Q
-                        </div>
-                        <div className = "Play-KeyTile">
-                            Q
-                        </div>
-                        <div className = "Play-KeyTile">
-                            Q
-                        </div>
-                        <div className = "Play-KeyTile">
-                            Q
-                        </div>
+                        {keyboardR2}
                     </div>
                     <div className = "Play-KeyboardRow3">
-                        <div className = "Play-KeyTile Back">
-                            Q
-                        </div>
-                        <div className = "Play-KeyTile">
-                            Q
-                        </div>
-                        <div className = "Play-KeyTile">
-                            Q
-                        </div>
-                        <div className = "Play-KeyTile">
-                            Q
-                        </div>
-                        <div className = "Play-KeyTile">
-                            Q
-                        </div>
-                        <div className = "Play-KeyTile">
-                            Q
-                        </div>
-                        <div className = "Play-KeyTile">
-                            Q
-                        </div>
-                        <div className = "Play-KeyTile">
-                            Q
-                        </div>
-                        <div className = "Play-KeyTile Enter">
-                            Q
-                        </div>
+                        {keyboardR3}
                     </div>
                 </div>
             </div>
