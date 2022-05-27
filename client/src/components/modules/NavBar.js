9766ef8791dfdc8a63af17ac7dcbbcde12c99cb2
Opeyemi Balogun <jpeyemi@MacBook-Pro-4.local>
2022-05-27 00:18:11 -0400

    keyboard continued

--- client/src/components/modules/NavBar.js
+++ client/src/components/modules/NavBar.js
@@ -6,11 +6,24 @@ import "./NavBar.css";
 import "../../utilities.css";
 
 const NavBar = (props) => {
-  
+  let title = ('FRIENDLE').split('');
+  let frien = title.slice(0,5).map((w) => (
+    <div className = "Nav-Logo green">
+        {w}
+    </div>   
+));
+
+let dle = title.slice(5).map((w) => (
+  <div className = "Nav-Logo yellow">
+      {w}
+  </div>   
+));
+
+let friendle = frien.concat(dle);
   return (
     <>
     <div className = "Nav-Container">
-        Friendle
+        {friendle}
     </div>
     </>
   );
